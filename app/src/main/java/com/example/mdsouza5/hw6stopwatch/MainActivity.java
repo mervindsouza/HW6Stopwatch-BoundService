8b81b3e1b05f8fa9cd8aebcf9301e81e74be3283
mdsouza5 <mdsouza5@hawk.iit.edu>
2018-03-19 09:21:15 -0500

    Stopwatch BoundServices Added

--- app/src/main/java/com/example/mdsouza5/hw6stopwatch/MainActivity.java
+++ app/src/main/java/com/example/mdsouza5/hw6stopwatch/MainActivity.java
@@ -1,16 +1,26 @@
 package com.example.mdsouza5.hw6stopwatch;
 
+import android.app.Service;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Handler;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     Context mainActivityContext;
+    Handler boundServiceHandler;
+    StopwatchService stopwatchServiceObj;
     Button startServicesButton;
     Button stopServicesButton;
     Button startTimerButton;
@@ -24,11 +34,40 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mainActivityContext = this.getBaseContext(); // may substitute with this
-        startServicesButton = (Button)findViewById(R.id.startServicesMA);
-        stopServicesButton  = (Button)findViewById(R.id.stopServicesMA);
-        startTimerButton = (Button)findViewById(R.id.startButtonMA);
-        stopTimerButton = (Button)findViewById(R.id.stopButtonMA);
-        resetTimerButton = (Button)findViewById(R.id.resetButtonMA);
-        showTimerTextView = (TextView)findViewById(R.id.showTimerMA);
+        boundServiceHandler = new Handler();
+        startServicesButton = (Button) findViewById(R.id.startServicesMA);
+        stopServicesButton = (Button) findViewById(R.id.stopServicesMA);
+        startTimerButton = (Button) findViewById(R.id.startButtonMA);
+        stopTimerButton = (Button) findViewById(R.id.stopButtonMA);
+        resetTimerButton = (Button) findViewById(R.id.resetButtonMA);
+        showTimerTextView = (TextView) findViewById(R.id.showTimerMA);
+
+        ServiceConnection serviceConnectionForBinding = new ServiceConnection() {
+            @Override
+            public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+                
+            }
+
+            @Override
+            public void onServiceDisconnected(ComponentName componentName) {
+
+            }
+        }
+
+        startServicesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Declare Intents
+                Intent intentForStopwatchService = new Intent(mainActivityContext.getApplicationContext(), StopwatchService.class);
+                try {
+                    startService(intentForStopwatchService);
+                    bindService(intentForStopwatchService, serviceConnectionForBinding, )
+                }catch (SecurityException secEx){
+                    Toast.makeText(getApplicationContext(), "Security Exception Occured!", Toast.LENGTH_LONG).show();
+                }catch(IllegalStateException illEx){
+                    Toast.makeText(getApplicationContext(), "Illegal State Exception Occured!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mdsouza5/hw6stopwatch/StopwatchService.java
+++ app/src/main/java/com/example/mdsouza5/hw6stopwatch/StopwatchService.java
@@ -1,10 +1,64 @@
 package com.example.mdsouza5.hw6stopwatch;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.os.SystemClock;
+import android.support.annotation.Nullable;
+import android.widget.Chronometer;
 
 /**
  * Created by merv on 3/18/18.
  */
 
-public class StopwatchService extends AppCompatActivity {
+public class StopwatchService extends Service {
+
+    protected Chronometer stopwatchChronometer;
+    protected IBinder iBinder = new StopWatchBinder();
+
+    public class StopWatchBinder extends Binder{
+        StopwatchService getServiceFromStopwatchService(){
+            return StopwatchService.this;
+        }
+    }
+
+    //Chronometer Methods
+
+    protected void StopChronomter(){
+        stopwatchChronometer.stop();
+    }
+
+    //Binder Methods
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return iBinder;
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        super.onRebind(intent);
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return super.onUnbind(intent);
+    }
+
+    //StopwatchService Methods
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        stopwatchChronometer = new Chronometer(this);
+        stopwatchChronometer.setBase(SystemClock.elapsedRealtime());
+        stopwatchChronometer.start();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopwatchChronometer.stop();
+    }
 }
